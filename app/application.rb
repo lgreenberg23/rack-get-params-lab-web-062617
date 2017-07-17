bf532182b0ebfa44ad326007bee7d7475303f28e
github username <github email address>
2017-07-17 08:58:35 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,9 @@
+require 'pry'
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
-
+  @@cart = []
+  
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
@@ -10,9 +12,28 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+
+    
+    elsif req.path.match(/cart/)
+      if @@cart.length >0
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      else
+        resp.write "Your cart is empty"
+      end
+
+    elsif req.path.match(/add/)
+      item = req.params["item"]  #I SEE THAT THIS IS COMING UP BLANK.. WHY?
+      # binding.pry
+      resp.write add_item(item)
+    
+    
+
     else
       resp.write "Path Not Found"
     end
@@ -27,4 +48,31 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
+
+  def add_item(item)
+    if !@@items.include?(item) == true
+        return "We don't have that item"
+    else       
+        @@cart << item
+        return "added #{item}"
+    end
+  end
+
+
 end
+
+
+
+# INSTRUCTIONS
+# Create a new class array called @@cart to hold any items in your cart
+# Create a new route called /cart to show the items in your cart
+# Create a new route called /add that takes in a GET param with the key item. 
+# This should check to see if that item is in @@items and then add it to the cart if 
+# it is.
+# Otherwise give an error
+
+
+
+
+
+
